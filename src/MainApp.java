9deeb2bf234c64be5716db0535c8bd1761158142
qwq140 <qwq140@gmail.com>
2020-12-18 15:10:52 +0900

    DAO

--- src/MainApp.java
+++ src/MainApp.java
@@ -6,104 +6,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 import config.DBConnection;
+import dao.DeptDao;
 import model.Dept;
 
 public class MainApp {
-
-	
-
-	public static void 추가(int id) {
-		String sql = "INSERT INTO test1(id) VALUES(?)"; // ;을 넣으면 오류뜸
-		Connection conn = DBConnection.getInstance();
-		try {
-			PreparedStatement pstmt = conn.prepareStatement(sql);
-			pstmt.setInt(1, id);
-			int result = pstmt.executeUpdate(); // 변경된 row count를 리턴, -1 값은 오류시에만 리턴
-			System.out.println("result : " + result);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	public static void 삭제(int id) {
-		String sql = "DELETE FROM test1 WHERE id = ?"; // ;을 넣으면 오류뜸
-		Connection conn = DBConnection.getInstance();
-		try {
-			PreparedStatement pstmt = conn.prepareStatement(sql);
-			pstmt.setInt(1, id);
-			int result = pstmt.executeUpdate(); // 변경된 row count를 리턴, -1 값은 오류시에만 리턴
-			System.out.println("result : " + result);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	public static void 수정(int id) {
-		String sql = "UPDATE test1 SET id = ?"; // ;을 넣으면 오류뜸
-		Connection conn = DBConnection.getInstance();
-		try {
-			PreparedStatement pstmt = conn.prepareStatement(sql);
-			pstmt.setInt(1, id);
-			int result = pstmt.executeUpdate(); // 변경된 row count를 리턴, -1 값은 오류시에만 리턴
-			System.out.println("result : " + result);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	public static Dept 찾기(int deptno) {
-		String sql = "SELECT deptno, dname, loc FROM dept WHERE deptno = ?"; // ;을 넣으면 오류뜸
-		Connection conn = DBConnection.getInstance();
-		try {
-			PreparedStatement pstmt = conn.prepareStatement(sql);
-			pstmt.setInt(1, deptno);
-			ResultSet rs = pstmt.executeQuery();
-			if (rs.next()) {
-				Dept dept = Dept.builder()
-						.deptno(rs.getInt("deptno"))
-						.dname(rs.getString("dname"))
-						.loc(rs.getString("loc"))
-						.build();
-				System.out.println(dept);
-				return dept;
-//				int deptno2 = rs.getInt("deptno"); // 변수에 넣는 이유는 실수를 방지하고 사용하기 편하기 때문에
-//				String dname = rs.getString("dname");
-//				String loc = rs.getString("loc");
-//				System.out.println(deptno2);
-//				System.out.println(dname);
-//				System.out.println(loc);
-			}
-			// next() 커서를 한칸 내림
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
-
-	public static List<Dept> 전체찾기() {
-		String sql = "SELECT deptno, dname, loc FROM dept"; // ;을 넣으면 오류뜸
-		Connection conn = DBConnection.getInstance();
-
-		try {
-			PreparedStatement pstmt = conn.prepareStatement(sql);
-			ResultSet rs = pstmt.executeQuery();
-			List<Dept> alDept = new ArrayList<>();
-			while (rs.next()) {
-
-				Dept dept = Dept.builder()
-						.deptno(rs.getInt("deptno"))
-						.dname(rs.getString("dname"))
-						.loc(rs.getString("loc")).build();
-
-				alDept.add(dept);	
-			}
-			return alDept;
-			// next() 커서를 한칸 내림
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
 
 	public static void main(String[] args) {
 //		추가(9);
@@ -111,7 +17,7 @@ public class MainApp {
 //		수정(9);
 //		Dept dept = 찾기(10);
 //		System.out.println(dept);
-		List<Dept> alDept =전체찾기();
+		List<Dept> alDept = DeptDao.전체찾기(); // 데이터 엑세스 오브젝트를 통해서 데이터베이스 접근
 		System.out.println(alDept);
 	}
 }
---
+++ src/dao/DeptDao.java
@@ -0,0 +1,107 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import config.DBConnection;
+import model.Dept;
+
+// Data Access Object (데이터에 접근하게 해주는 객체) - 재사용하기 위해 만듬
+public class DeptDao {
+	
+	public static void 추가(int id) {
+		String sql = "INSERT INTO test1(id) VALUES(?)"; // ;을 넣으면 오류뜸
+		Connection conn = DBConnection.getInstance();
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, id);
+			int result = pstmt.executeUpdate(); // 변경된 row count를 리턴, -1 값은 오류시에만 리턴
+			System.out.println("result : " + result);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void 삭제(int id) {
+		String sql = "DELETE FROM test1 WHERE id = ?"; // ;을 넣으면 오류뜸
+		Connection conn = DBConnection.getInstance();
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, id);
+			int result = pstmt.executeUpdate(); // 변경된 row count를 리턴, -1 값은 오류시에만 리턴
+			System.out.println("result : " + result);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void 수정(int id) {
+		String sql = "UPDATE test1 SET id = ?"; // ;을 넣으면 오류뜸
+		Connection conn = DBConnection.getInstance();
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, id);
+			int result = pstmt.executeUpdate(); // 변경된 row count를 리턴, -1 값은 오류시에만 리턴
+			System.out.println("result : " + result);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static Dept 찾기(int deptno) {
+		String sql = "SELECT deptno, dname, loc FROM dept WHERE deptno = ?"; // ;을 넣으면 오류뜸
+		Connection conn = DBConnection.getInstance();
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, deptno);
+			ResultSet rs = pstmt.executeQuery();
+			if (rs.next()) {
+				Dept dept = Dept.builder()
+						.deptno(rs.getInt("deptno"))
+						.dname(rs.getString("dname"))
+						.loc(rs.getString("loc"))
+						.build();
+				System.out.println(dept);
+				return dept;
+//				int deptno2 = rs.getInt("deptno"); // 변수에 넣는 이유는 실수를 방지하고 사용하기 편하기 때문에
+//				String dname = rs.getString("dname");
+//				String loc = rs.getString("loc");
+//				System.out.println(deptno2);
+//				System.out.println(dname);
+//				System.out.println(loc);
+			}
+			// next() 커서를 한칸 내림
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public static List<Dept> 전체찾기() {
+		String sql = "SELECT deptno, dname, loc FROM dept"; // ;을 넣으면 오류뜸
+		Connection conn = DBConnection.getInstance();
+
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			ResultSet rs = pstmt.executeQuery();
+			List<Dept> alDept = new ArrayList<>();
+			while (rs.next()) {
+
+				Dept dept = Dept.builder()
+						.deptno(rs.getInt("deptno"))
+						.dname(rs.getString("dname"))
+						.loc(rs.getString("loc")).build();
+
+				alDept.add(dept);	
+			}
+			return alDept;
+			// next() 커서를 한칸 내림
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+}
